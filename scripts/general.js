5dc6a84cb5162963b5c0ff28d05f30ed88203cb0
amir.rismaani <amir.rismaani@gmail.com>
2022-01-15 21:13:49 +0330

    Assemble section

--- scripts/general.js
+++ scripts/general.js
@@ -3,3 +3,38 @@ hamburger.addEventListener('click', function(){
     this.classList.toggle('hamburger--open');
     this.parentNode.querySelector(".menu").classList.toggle('menu--open');
 });
+
+
+const tabs = document.querySelectorAll('.tab');
+const tabPanes = document.querySelectorAll('.tab-pane');
+
+let tabInterval = null;
+initTabInterval();
+
+tabs.forEach( tab => {
+    tab.addEventListener('click', () => {
+        clearInterval(tabInterval);
+        const targetTabPane = document.querySelector(`#${tab.dataset.targetId}`)
+        
+        tabs.forEach(tab => {
+            tab.classList.remove('active');
+        });
+        tab.classList.add('active');
+
+        tabPanes.forEach(tabPane => {
+            tabPane.classList.remove('in');
+        });
+        targetTabPane.classList.add('in');
+        initTabInterval();
+    })
+})
+
+function initTabInterval(){
+    tabInterval = setInterval(() => {
+        let activeTab = document.querySelector('.tab.active');
+        let nextTab = activeTab.nextElementSibling;
+        if(!nextTab)
+            nextTab = activeTab.parentNode.firstElementChild;
+        nextTab.click();
+    }, 5000);
+}
